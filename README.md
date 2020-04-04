c2f87c74bfd912c8b7ec246e4ba1e2244e7dfe03
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-04-04 14:04:32 +0700

    learning loop-fo-JS

---
+++ README.md
@@ -0,0 +1,4 @@
+# week 2 of learning web development
+
+## Challange loop for JS
+
---
+++ index.js
@@ -0,0 +1,73 @@
+//factorial loop
+
+// let inputNumber = prompt('Please enter an integer')
+// let total= 1;
+// for (let i = 0; i < inputNumber; i++) {
+//     total = total * (inputNumber - i);
+// console.log (inputNumber + '! =' + total )
+// }
+
+
+
+// reverse string
+
+// let iterable = 'ayam';
+// let reversed = "";
+// for (let value of iterable) {
+//     reversed = value + reversed;
+// }
+// console.log(reversed);
+
+
+
+//reverse array
+
+// function reverseArray (arr) {
+//     let newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         newArr[i] = arr[arr.length - i - 1];
+//     }
+//     return newArr;
+// }
+// console.log(reverseArray(['apple', 'melon', 'mango']));
+
+
+//assign grade using for
+
+// function nilai(arr) {
+
+//         if (arr >=80 ) {
+//             return 'A';
+//         } else if (arr >=65) {
+//             return 'B';
+//         } else if (arr >= 55) {
+//             return 'C';
+//         } else if (arr >=40) {
+//             return 'D';
+//         } else {
+//             return 'F';
+//         }
+       
+//     }
+    
+//     for (let i = 1; i <= 100; i++) {
+//         console.log(i + nilai(i));
+        
+// }
+
+
+
+//multiplier using loop for
+
+let multiplier = 8;
+for (var i = 0; i <= 10; i++) {
+    var result = multiplier * i;
+    console.log(multiplier + ' * ' + i + ' = ' + result);
+}
+
+
+
+
+
+
+
